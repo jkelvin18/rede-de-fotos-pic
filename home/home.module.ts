ded120fbffdef173a32dbc1f074dbdc72c26a299
James <jkelvin18@hotmail.com>
2019-10-09 23:47:01 -0300

    inserido as validacoes de formulario

--- home/home.module.ts
+++ home/home.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    declarations: [ SignInComponent]
+    declarations: [ SignInComponent],
+    imports: [ 
+        ReactiveFormsModule,
+        CommonModule],
 })
 
 export class HomeModule { }
--- home/signin/signin.component.ts
+++ home/signin/signin.component.ts
@@ -1,6 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './sigin.component.html'
 })
-export class SignInComponent { }
+export class SignInComponent implements OnInit{
+    
+    loginForm: FormGroup;
+    
+    constructor(private formBuilder: FormBuilder) { }
+    
+    ngOnInit(): void {
+        
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        }); 
+
+    }
+ }
