eb9991bbf58e7e76299fd6505815489e19421ad1
James <jkelvin18@hotmail.com>
2019-10-12 00:13:58 -0300

    adicionado a componente de validacao de usuario

--- home/home.module.ts
+++ home/home.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [ SignInComponent],
     imports: [ 
         ReactiveFormsModule,
-        CommonModule],
+        CommonModule,
+        VMessageModule,
+        RouterModule
+    ]
 })
 
 export class HomeModule { }
--- home/signin/signin.component.ts
+++ home/signin/signin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/auth/platform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './sigin.component.html'
@@ -7,15 +10,38 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignInComponent implements OnInit{
     
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
     
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService) { }
     
     ngOnInit(): void {
         
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
-        }); 
+        });    
+    }
 
+    login() {
+
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.group(err);
+                    this.loginForm.reset();
+                    this.platformDetectorService.isPlatformBrowser() &&
+                        this.userNameInput.nativeElement.focus();
+                    alert('invalid user name or password');
+                }
+            );
     }
  }
